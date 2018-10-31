8ddd93d26b6e9fe1fa2f687bcad6453c4844c9e0
hjx <huangjiaxin2@yy.com>
2018-10-31 17:16:21 +0800

    Add Android demo project.

---
+++ AndroidDemo/app/src/main/java/com/demo/luayoga/yy/androiddemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demo.luayoga.yy.androiddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
