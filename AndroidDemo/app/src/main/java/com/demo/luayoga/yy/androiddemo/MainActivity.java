5b44636e36d2a40d0b3d6306304df88866c58fcd
hjx <huangjiaxin2@yy.com>
2018-11-01 17:17:12 +0800

    Embedding the yoga

--- AndroidDemo/app/src/main/java/com/demo/luayoga/yy/androiddemo/MainActivity.java
+++ AndroidDemo/app/src/main/java/com/demo/luayoga/yy/androiddemo/MainActivity.java
@@ -3,11 +3,17 @@ package com.demo.luayoga.yy.androiddemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.yoga.YogaNode;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        YogaNode root = new YogaNode();
+        root.setWidth(300);
+        root.setHeight(500);
     }
 }
