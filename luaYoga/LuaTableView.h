dee59f73a5cb1800e70b8b634dc805973ae7ed94
wenzhining <wenzhining@yy.com>
2018-11-06 19:19:17 +0800

    add lua table view

---
+++ luaYoga/LuaTableView.h
@@ -0,0 +1,17 @@
+//
+//  LuaTableView.h
+//  luaYoga
+//
+//  Created by wen william on 2018/11/6.
+//  Copyright © 2018年 wen william. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LuaTableView : UITableView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ luaYoga/LuaTableView.m
@@ -0,0 +1,21 @@
+//
+//  LuaTableView.m
+//  luaYoga
+//
+//  Created by wen william on 2018/11/6.
+//  Copyright © 2018年 wen william. All rights reserved.
+//
+
+#import "LuaTableView.h"
+
+@implementation LuaTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
