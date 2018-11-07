662a9c252c9b8cee453091889ce19200b53724e4
wenzhining <wenzhining@yy.com>
2018-11-07 11:02:52 +0800

    update

--- luaYoga/LuaTableView.mm
+++ luaYoga/LuaTableView.mm
@@ -7,24 +7,90 @@
 //
 
 #import "LuaTableView.h"
+@interface LuaTableView()<UITableViewDataSource, UITableViewDelegate>
+
+@end
 
 @implementation LuaTableView
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.delegate = self;
+        self.dataSource = self;
+    }
+    return self;
+}
 
 -(instancetype)init
 {
     self = [super init];
     if (self) {
-        
+        self.delegate = self;
+        self.dataSource = self;
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
 }
-*/
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString * identifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld-%ld",(long)indexPath.section , (long)indexPath.row];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50;
+}
+
+-(CGFloat)headerHeightForSection:(NSInteger)section
+{
+    return 10;
+}
+
+-(CGFloat)footerHeightForSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return [self headerHeightForSection:section];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return [self footerHeightForSection:section];
+}
+
+- (nullable UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView * view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, [self headerHeightForSection:section])];
+    view.backgroundColor = [UIColor redColor];
+    return view;
+}
+
+- (nullable UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    UIView * view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, [self footerHeightForSection:section])];
+    view.backgroundColor = [UIColor greenColor];
+    return view;
+}
 
 @end
