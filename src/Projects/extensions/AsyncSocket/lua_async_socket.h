fa5c0263a635637dedf61b2e3384425962720dfd
wenzhining <wenzhining@yy.com>
2018-11-20 18:22:58 +0800

    更新命名规则

--- src/Projects/extensions/AsyncSocket/lua_async_socket.h
+++ src/Projects/extensions/AsyncSocket/lua_async_socket.h
@@ -1,8 +1,9 @@
+
 #pragma once
 extern "C" {
 #include "lua.h"
 }
-#define LUA_ASYNC_SOCKET_METATABLE_NAME "lua.asyncSocket"
+#define LUA_ASYNC_SOCKET_METATABLE_NAME "lua_asyncSocket"
 
 extern int luaopen_async_socket(lua_State* L);
 
